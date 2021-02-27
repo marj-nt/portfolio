f5222e3105c1fe4c6136ad2076da34d3b51bc294
Marjorie Tolentino <marjorientolentino@gmail.com>
2021-02-26 16:00:31 -0800

    Small nav

--- js/scrolling.js
+++ js/scrolling.js
@@ -51,22 +51,67 @@ function onScroll(event){
 
     var mainOpen = false;
 
-    $('.hamburger-icon').click(function() {
+    $('.sidenav .hamburger-icon').click(function() {
         console.log('test')
         if (mainOpen === false) {
             $('.project-nav').fadeOut();
             
             $('.main-nav').fadeIn();
-            $('.hamburger-icon').html('&#10005;');
+            $('.sidenav .hamburger-icon').html('&#10005;');
             mainOpen = true;
         } else if(mainOpen === true) {
             $('.main-nav').fadeOut();
             $('.project-nav').fadeIn();
-            $('.hamburger-icon').html('&#9776;');
+            $('.sidenav .hamburger-icon').html('&#9776;');
        
             mainOpen = false;
         }
     })
+
+    var navOpen = false;
+
+    var activeMenu = 1;
+
+    $('.hamburger-sm').click(function() {
+        if(navOpen === false) {
+          $('.sidenav-sm').addClass('open');
+          $('.sidenav-sm').css('height', '75vh');
+          $('.sidenav-sm .hamburger-sm').html('&#10005;');
+          $('.sidenav-sm .active-menu').fadeIn();
+          $('.sidenav-sm .project-nav').fadeIn();
+          navOpen = true;
+        } else if(navOpen === true) {
+            $('.sidenav-sm').css('height', 'auto');
+            $('.sidenav-sm .active-menu').fadeOut();
+          $('.sidenav-sm .project-nav').fadeOut();
+          $('.sidenav-sm').removeClass('open');
+          $('.sidenav-sm .hamburger-sm').html('&#9776;');
+
+          navOpen = false;
+          activeMenu = 1;
+
+          $('.sidenav-sm .project-nav').fadeOut()
+            $('.sidenav-sm .main-nav').fadeOut()
+            $('.active-menu').html('&#8594;MAIN NAV')
+        }
+
+    })
+
+    $('.active-menu').click(function() {
+        if(activeMenu === 1) {
+            $('.sidenav-sm .project-nav').fadeOut()
+            $('.sidenav-sm .main-nav').fadeIn()
+            $('.active-menu').html('&#8592;PROJECT NAV')
+            activeMenu = 2;
+        } else if (activeMenu === 2) {
+            $('.sidenav-sm .project-nav').fadeIn()
+            $('.sidenav-sm .main-nav').fadeOut()
+            $('.active-menu').html('&#8594;MAIN NAV')
+            activeMenu = 1;
+        }
+    })
+
+
 
 
 
